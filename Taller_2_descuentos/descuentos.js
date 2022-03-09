89b69c6b79d13366164335a1ccc4d53ef88450df
Arnulfo Garcia <harngalf@hotmail.com>
2022-03-09 13:06:47 -0600

    Terminamos el taller de Descuentos, y aprendimos a usar .innerText para imprimi en el html

---
+++ Taller_2_descuentos/descuentos.js
@@ -0,0 +1,26 @@
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDescount(){
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("resultPrice");
+    const resultB = document.getElementById("resultBlack");
+
+    resultP.innerText = "El precio con descuento es de: "; 
+    resultB.innerText = "$ " + precioConDescuento + " pesos (MXP)";
+
+};
+
+
+
+
